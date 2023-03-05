b4188b742a590ee5d5980d4a6d62b2bcf7f70087
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-03-05 17:34:52 +0200

    bisection method

---
+++ bisection.py
@@ -0,0 +1,36 @@
+import math as m
+
+error = 100
+xr_old = 0
+
+xl = float(input("Enter the lower limit: "))
+xu = float(input("Enter the upper limit: "))
+
+
+
+while error > 0.001:
+    xr_new = (xl + xu)/2
+    
+    fxl = m.pow((m.pow(xl, 2)+1), 2)-25
+    fxr = m.pow((m.pow(xr_new, 2)+1), 2)-25
+
+    mul_fxl_fxr = fxl*fxr
+    
+    if mul_fxl_fxr < 0:
+        xu = xr_new
+    elif mul_fxl_fxr > 0:
+        xl = xr_new
+
+    if xr_old != 0:
+        error = ((xr_new - xr_old)/xr_new) * 100
+        
+        if(error < 0):
+            error = error * -1
+        else:
+            error = error
+        
+    xr_old = xr_new
+
+
+print("The root is: ", xr_new)
+print("The error is: ", error)
